481b7611d17834fa6cc854224395414a2cc4d5d2
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-17 02:30:42 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,9 +2,10 @@ require 'pry'
 
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
+   collection << yield(array[i])
     binding.pry
-    yield(array[i])
     i += 1
   end
 end
